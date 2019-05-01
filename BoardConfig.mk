91852659b537356244932b18a0c22d57f31202af
Soft-Bullet <40681532+Soft-Bullet@users.noreply.github.com>
2019-05-01 15:49:06 +0545

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,18 +1,18 @@
 # Inherit from common
-include device/samsung/j5-common/BoardConfigCommon.mk
+include device/samsung/a5-common/BoardConfigCommon.mk
 
-LOCAL_PATH := device/samsung/j53gxx
+LOCAL_PATH := device/samsung/a5ultexx
 
 # Asserts
-TARGET_OTA_ASSERT_DEVICE := j53gxx,j53g
+TARGET_OTA_ASSERT_DEVICE := a5ulte,a5ultexx,a5lte,a5ltexx,a5lte,a53gxx,a53g
 
 # Kernel
-TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_j53g_eur_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_a5u_eur_defconfig
 
 # Partition sizes
-BOARD_SYSTEMIMAGE_PARTITION_SIZE    := 2097152000
-BOARD_USERDATAIMAGE_PARTITION_SIZE  := 5079281664
+BOARD_SYSTEMIMAGE_PARTITION_SIZE    := 2336096256
+BOARD_USERDATAIMAGE_PARTITION_SIZE  := 12775813120
 
 # RIL
 SIM_COUNT := 2
-BOARD_GLOBAL_CFLAGS += -DRIL_FIX_SMS_NOT_SENT_ERR
+
